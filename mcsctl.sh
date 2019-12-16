53d8d4dd7ce32968b3b95b56291f25aee0795828
Hetsh <git@hetsh.de>
2019-12-16 18:06:43 +0100

    Fixes #1 #23 #24 -> Target all servers at once with "all" as server id, minor improvements

--- mcsctl.sh
+++ mcsctl.sh
@@ -8,7 +8,6 @@ readonly SERVER_COMMAND="$3"
 
 # Commands
 readonly CMD_HELP="help"
-readonly CMD_LIST="list"
 readonly CMD_STATUS="status"
 readonly CMD_START="start"
 readonly CMD_STOP="stop"
@@ -35,6 +34,9 @@ readonly ERROR_SERVER_ACTIVE="10"
 readonly ERROR_SERVER_INACTIVE="11"
 readonly ERROR_EULA_FILE_MISSING="12"
 readonly ERROR_PROPERTIES_FILE_MISSING="13"
+readonly ERROR_MULTI_CONSOLE="14"
+readonly ERROR_MULTI_CREATE="15"
+readonly ERROR_NO_SERVERS="16"
 # /Results
 
 # Enforce mcs user (except for help command)
@@ -50,14 +52,13 @@ MAX_RAM="1024"
 TIMEOUT="10"
 SERVER_ROOT="$HOME"
 DATE_FORMAT="%Y_%m_%d-%H_%M_%S"
-if [ -n "$SERVER_ID" ]; then
-INITIAL_SERVER_PROPERTIES="server-port=$((25564 + $SERVER_ID))
-motd=Welcome to MC-Server #$SERVER_ID.
-player-idle-timeout=5
-snooper-enabled=false
-view-distance=15"
+if [ -n "$SERVER_ID" ] && [ "$SERVER_ID" != "all" ]; then
+	INITIAL_SERVER_PROPERTIES="server-port=$((25564 + $SERVER_ID))
+	\rmotd=Welcome to MC-Server #$SERVER_ID.
+	\rplayer-idle-timeout=5
+	\rsnooper-enabled=false
+	\rview-distance=15"
 fi
-
 # /Mutable config
 source "/etc/mcsctl.conf" &> /dev/null
 
@@ -68,11 +69,6 @@ readonly SERVER_APP_NAME="server.jar"
 readonly SERVER_APP="$SERVER_DIR/$SERVER_APP_NAME"
 readonly SERVER_VERSION="$SERVER_DIR/server.version"
 # /Immutable config
-
-
-custom_date() {
-	date "+$DATE_FORMAT"
-}
 
 screen_active() {
 	if [ -n "$(screen -list | grep -o "$SERVER_NAME")" ]; then
@@ -116,32 +112,17 @@ wait_server_stop() {
 	done
 }
 
-list() {
-	SERVER_LIST=$(find "$SERVER_ROOT" -type f -name "$SERVER_APP_NAME" | sort -n)
-	echo "Total servers: $(echo "$SERVER_LIST" | wc -w)"
-	for SERVER in $SERVER_LIST; do
-		SERVER=${SERVER##*mcserver}
-		SERVER=${SERVER%%/*}
-		echo "MCServer #$SERVER"
-	done
-}
-
 status() {
-	if screen_active; then
-		echo -n $(custom_date) "Screen active, "
+	echo -n $(date "+$DATE_FORMAT:") "MCServer #$SERVER_ID: Server "
+	if server_active || screen_active; then
+		echo "active"
 	else
-		echo -n "Screen inactive, "
-	fi
-
-	if server_active; then
-		echo "Server active"
-	else
-		echo "Server inactive"
+		echo "inactive"
 	fi
 }
 
 start() {
-	echo -n $(custom_date) "Starting server... "
+	echo -n $(date "+$DATE_FORMAT:") "Starting server... "
 
 	# start screen session
 	if ! screen_active; then
@@ -159,7 +140,7 @@ start() {
 }
 
 stop() {
-	echo -n $(custom_date) "Stopping server... "
+	echo -n $(date "+$DATE_FORMAT:") "Stopping server... "
 
 	# stop server application with timeout
 	if server_active; then
@@ -179,7 +160,7 @@ stop() {
 }
 
 console() {
-	echo -n $(custom_date) "Connecting to screen session... "
+	echo -n $(date "+$DATE_FORMAT:") "Connecting to screen session... "
 
 	screen -r "$SERVER_NAME"
 
@@ -187,7 +168,7 @@ console() {
 }
 
 forward_command() {
-	echo -n $(custom_date) "Forwarding command to server... "
+	echo -n $(date "+$DATE_FORMAT:") "Forwarding command to server... "
 
 	screen -S "$SERVER_NAME" -p 0 -X stuff "$SERVER_COMMAND\n"
 	local KEYWORD="/\[$(date +%H:%M:%S)\]/p"
@@ -198,8 +179,6 @@ forward_command() {
 }
 
 download() {
-	echo -n $(custom_date) "Downloading latest version... "
-
 	local CURRENT_VERSION="$(cat "$SERVER_VERSION" 2> /dev/null)"
 	local LATEST_VERSION=$(curl -s -L "https://launchermeta.mojang.com/mc/game/version_manifest.json" | jq -r ".latest.release")
 	if [ $(vercmp "$CURRENT_VERSION" "$LATEST_VERSION") -ge 0 ]; then
@@ -220,12 +199,12 @@ download() {
 		exit $ERROR_DOWNLOAD_FAILED
 	fi
 	echo "$LATEST_VERSION" > "$SERVER_VERSION"
-
-	echo "done"
 }
 
-configure() {
-	echo -n $(custom_date) "Configuring server... "
+create() {
+	echo -n $(date "+$DATE_FORMAT:") "Creating server... "
+	
+	download
 
 	# Just accept the EULA, you wouldn't read it anyway :P
 	echo "eula=TRUE" > "$SERVER_DIR/eula.txt"
@@ -236,8 +215,16 @@ configure() {
 	echo "done"
 }
 
+update() {
+	echo -n $(date "+$DATE_FORMAT:") "Updating server... "
+
+	download
+
+	echo "done"
+}
+
 remove() {
-	echo -n $(custom_date) "Removing server... "
+	echo -n $(date "+$DATE_FORMAT:") "Removing server... "
 
 	rm -r -f "$SERVER_DIR"
 
@@ -246,30 +233,29 @@ remove() {
 
 help() {
 	local MY_NAME="${0##*/}"
-	echo -e "Usage: $MY_NAME {$CMD_HELP|$CMD_STATUS|$CMD_START|$CMD_STOP|$CMD_RESTART|$CMD_CREATE|$CMD_UPDATE|$CMD_DESTROY}
+	echo -e "Usage: $MY_NAME {$CMD_HELP|$CMD_STATUS|$CMD_START|$CMD_STOP|$CMD_RESTART|$CMD_CONSOLE|$CMD_COMMAND|$CMD_CREATE|$CMD_UPDATE|$CMD_DESTROY}
 		\r$CMD_HELP			Prints this help.
-		\r$CMD_LIST			Shows all existing servers
-		\r$CMD_STATUS	<id>		Status of a server and its screen session.
-		\r$CMD_START	<id>		Starts a server inside a screen session.
-		\r$CMD_STOP	<id>		Stops a server and its screen session.
-		\r$CMD_RESTART	<id>		Restarts a server.
-		\r$CMD_CONSOLE	<id>		Connect to the screen session.
-		\r$CMD_COMMAND	<id> <cmd>	Forward <cmd> to specified server.
-		\r$CMD_CREATE	<id>		Creates a server in \"$SERVER_DIR\".
-		\r$CMD_UPDATE	<id>		Downloads a new minecraft server executable for the specified server.
-		\r$CMD_DESTROY	<id>		Removes all files of a server."
+		\r$CMD_STATUS	<id/all>	Lists status of server(s).
+		\r$CMD_START	<id/all>	Starts server(s) inside screen session(s).
+		\r$CMD_STOP	<id/all>	Stops server(s) and screen session(s).
+		\r$CMD_RESTART	<id/all>	Restart server(s).
+		\r$CMD_CONSOLE	<id>		Connect to the screen session of a server.
+		\r$CMD_COMMAND	<id/all> <cmd>	Forward <cmd> to specified server(s).
+		\r$CMD_CREATE	<id>		Creates a server in the configured SERVER_ROOT (default: /home/$MCS_USER).
+		\r$CMD_UPDATE	<id/all>	Downloads a new minecraft server executable for server(s).
+		\r$CMD_DESTROY	<id/all>	Removes all files of server(s)."
 }
 
 require_server_id() {
 	if [ -z "$SERVER_ID" ]; then
-		echo $(custom_date) "Missing server id!"
+		echo $(date "+$DATE_FORMAT:") "Missing server id!"
 		exit $ERROR_ID_MISSING
 	fi
 }
 
 require_command() {
 	if [ -z "$SERVER_COMMAND" ]; then
-		echo $(custom_date) "Missing command!"
+		echo $(date "+$DATE_FORMAT:") "Missing command!"
 		exit $ERROR_COMMAND_MISSING
 	fi
 }
@@ -278,17 +264,8 @@ require_server_exists() {
 	require_server_id
 	
 	if [ ! -d "$SERVER_DIR" ]; then
-		echo $(custom_date) "Server does not exist!"
+		echo $(date "+$DATE_FORMAT:") "Server does not exist!"
 		exit $ERROR_SERVER_MISSING
-	fi
-}
-
-require_server_missing() {
-	require_server_id
-	
-	if [ -d "$SERVER_DIR" ]; then
-		echo $(custom_date) "Server already exists!"
-		exit $ERROR_SERVER_EXISTS
 	fi
 }
 
@@ -296,7 +273,7 @@ require_server_active() {
 	require_server_exists
 	
 	if ! server_active; then
-		echo "Server is not running!"
+		echo $(date "+$DATE_FORMAT:") "Server is not running!"
 		exit $ERROR_SERVER_ACTIVE
 	fi
 }
@@ -305,65 +282,92 @@ require_server_inactive() {
 	require_server_exists
 	
 	if server_active; then
-		echo "Server is running!"
+		echo $(date "+$DATE_FORMAT:") "Server is running!"
 		exit $ERROR_SERVER_INACTIVE
 	fi
 }
 
 
-case "$CMD" in
-	"$CMD_HELP")
-		help
-		;;
-	"$CMD_LIST")
-		list
-		;;
-	"$CMD_STATUS")
-		require_server_exists
-		status
-		;;
-	"$CMD_START")
-		require_server_inactive
-		start
-		;;
-	"$CMD_STOP")
-		require_server_active
-		stop
-		;;
-	"$CMD_RESTART")
-		require_server_active
-		stop
-		start
-		;;
-	"$CMD_CONSOLE")
-		require_server_active
-		console
-		;;
-	"$CMD_COMMAND")
-		require_server_active
-		require_command
-		forward_command
-		;;
-	"$CMD_CREATE")
-		require_server_missing
-		download
-		configure
-		;;
-	"$CMD_UPDATE")
-		require_server_inactive
-		download
-		;;
-	"$CMD_DESTROY")
-		require_server_inactive
-		read -p "Delete world data and configuration of server #$SERVER_ID? [y/n]" -n 1 -r; echo ""
-		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			remove
-		fi
-		;;
-	*)
-		help
-		exit $ERROR_UNKNOWN_COMMAND
-		;;
-esac
+if [ "$SERVER_ID" == "all" ]; then
+	case "$CMD" in
+		"$CMD_CONSOLE")
+			echo $(date "+$DATE_FORMAT:") "Cannot connect to multiple consoles!"
+			exit $ERROR_MULTI_CONSOLE
+			;;
+		"$CMD_CREATE")
+			echo $(date "+$DATE_FORMAT:") "Cannot create multiple servers!"
+			exit $ERROR_MULTI_CREATE
+			;;
+	esac
+
+	SERVER_LIST=$(find "$SERVER_ROOT" -type f -name "$SERVER_APP_NAME" | sort -n)
+	if [ -z "$SERVER_LIST" ]; then
+		echo $(date "+$DATE_FORMAT:") "No servers exist!"
+		exit $ERROR_NO_SERVERS
+	fi
+	
+	for NEXT_ID in $SERVER_LIST; do
+		NEXT_ID=${NEXT_ID##*mcserver}
+		NEXT_ID=${NEXT_ID%%/*}
+		"$0" "$CMD" "$NEXT_ID" "$SERVER_COMMAND"
+	done
+	exit $SUCCESS
+else
+	case "$CMD" in
+		"$CMD_HELP")
+			require_server_exists
+			help
+			;;
+		"$CMD_STATUS")
+			status
+			;;
+		"$CMD_START")
+			require_server_inactive
+			start
+			;;
+		"$CMD_STOP")
+			require_server_active
+			stop
+			;;
+		"$CMD_RESTART")
+			require_server_active
+			stop
+			start
+			;;
+		"$CMD_CONSOLE")
+			require_server_active
+			console
+			;;
+		"$CMD_COMMAND")
+			require_server_active
+			require_command
+			forward_command
+			;;
+		"$CMD_CREATE")
+			require_server_id
+			if [ ! -d "$SERVER_DIR" ]; then
+				create
+			else
+				echo $(date "+$DATE_FORMAT:") "Server already exists!"
+				exit $ERROR_SERVER_EXISTS
+			fi
+			;;
+		"$CMD_UPDATE")
+			require_server_inactive
+			update
+			;;
+		"$CMD_DESTROY")
+			require_server_inactive
+			read -p "Delete world data and configuration of server #$SERVER_ID? [y/n]" -n 1 -r; echo ""
+			if [[ $REPLY =~ ^[Yy]$ ]]; then
+				remove
+			fi
+			;;
+		*)
+			help
+			exit $ERROR_UNKNOWN_COMMAND
+			;;
+	esac
+fi
 
 exit $SUCCESS
